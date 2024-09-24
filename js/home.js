563ff2621cf94edc0f52553e240e41ad481cc431
Mohammed-Masum-Billah <masumfahim41@gamil.com>
2024-09-24 17:07:26 +0600

    Finding problems

--- js/home.js
+++ js/home.js
@@ -1,4 +1,7 @@
 /* Steps
+
+
+
 - 
 */
 // Add money
@@ -6,25 +9,21 @@ document.getElementById('btn-add-money').addEventListener('click',function (even
     event.preventDefault();
 
     const addAmountinput = document.getElementById('input-add-amount').value;
-    console.log(addAmountinput);
+    // console.log(addAmountinput);
     const pinNumberinput = document.getElementById('input-pin-numb').value;
-    console.log(pinNumberinput);
+    // console.log(pinNumberinput);
 
     if (pinNumberinput=== '1234') {
-        console.log('Money added');
+        // console.log('Money added');
         const balance=document.getElementById('acct-balance').innerText;
-        console.log(balance);
+        // console.log(balance);
         
         const addMoneyNumber=parseFloat(addAmountinput);
         const addBalance=parseFloat(balance);
         const newBalance= addMoneyNumber + addBalance;
-        console.log(newBalance);
+        // console.log(newBalance);
         
         document.getElementById('acct-balance').innerText=newBalance;
-
-
-        document.getElementById('add-money-form').classList.remove('hidden')
-        document.getElementById('cash-out-form').classList.add('hidden')
     
     } 
     else {
@@ -38,27 +37,34 @@ document.getElementById('btn-cash-out').addEventListener('click',function (event
     event.preventDefault();
 
     const cashOutinput = document.getElementById('input-cash-out').value;
-    console.log(cashOutinput);
+    // console.log(cashOutinput);
     const pinNumberInput = document.getElementById('input-pin-numbs').value;
-    console.log(pinNumberInput);
+    // console.log(pinNumberInput);
 
     if (pinNumberInput=== '1234') {
         console.log('Money deduct');
         const balance=document.getElementById('acct-balance').innerText;
-        console.log(balance);
+        // console.log(balance);
         
         const subMoneyNumber=parseFloat(cashOutinput);
         const addBalance=parseFloat(balance);
         const newBalance= addBalance - subMoneyNumber;
-        console.log(newBalance);
+        // console.log(newBalance);
         
         document.getElementById('acct-balance').innerText=newBalance;
-
-
-        document.getElementById('cash-out-form').classList.remove('hidden')
-        document.getElementById('add-money-form').classList.add('hidden')
     } 
     else {
         console.log(' Please Try again later.');
     }
+})
+
+document.getElementById('cash-out-btn').addEventListener('click',function () {
+    document.getElementById('cash-out-form').classList.remove('hidden');
+    document.getElementById('add-money-form').classList.add('hidden')
+})
+
+document.getElementById('add-money-btn').addEventListener('clicked',function () {
+    document.getElementById('add-money-form'),classList.remove('hidden')
+    document.getElementById('cash-out-form'),classList.remove('hidden')
+    
 })
